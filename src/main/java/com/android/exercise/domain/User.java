96d20324a47e866a11d8f035feb1a16c77b5acaa
jmk307 <jimin112688@gmail.com>
2022-07-23 03:19:17 +0900

    refactor : test

--- src/main/java/com/android/exercise/domain/User.java
+++ src/main/java/com/android/exercise/domain/User.java
@@ -1,19 +1,25 @@
 package com.android.exercise.domain;
 
-import lombok.*;
 import org.springframework.data.annotation.Id;
 
-@Getter
-@Setter
-@NoArgsConstructor
 public class User {
     @Id
     private String id;
     private String password;
 
+    public User() {}
+
     public User(String userId, String password) {
         this.id = userId;
         this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public String toString() {
