553961b4963a10829f20d51ab4fb3781433650fc
jmk307 <jimin112688@gmail.com>
2022-07-23 02:31:19 +0900

    refactor : test

--- src/main/java/com/android/exercise/controller/api/HomeController.java
+++ src/main/java/com/android/exercise/controller/api/HomeController.java
@@ -3,16 +3,18 @@ package com.android.exercise.controller.api;
 import com.android.exercise.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "android")
 public class HomeController {
 
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @GetMapping
+    public String test() {
+        return "성공!!!!";
+    }
 
     @PostMapping
     public String androidResponse(@RequestBody User user) {
